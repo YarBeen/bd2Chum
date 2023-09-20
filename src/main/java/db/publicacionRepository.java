84db4acc64a6785c8f099dfd23d6b652b220e2af
Yarman Charpentier <yarman2002@gmail.com>
2023-09-20 15:12:49 -0600

    FALSDFLJEWSD

--- src/main/java/db/publicacionRepository.java
+++ src/main/java/db/publicacionRepository.java
@@ -109,7 +109,7 @@ public class publicacionRepository {
 
             CallableStatement callableStatement = connect.prepareCall(INSERT_PUBLICACION_QUERY);
 
-            callableStatement.setInt(1, entity.getId());
+            callableStatement.setInt(1, entity.getIdProducto());
             callableStatement.setString(2, entity.getTitulo());
             callableStatement.setString(3, entity.getEstado());
             callableStatement.setInt(4, entity.getPrecio());
@@ -180,7 +180,7 @@ public class publicacionRepository {
 
             CallableStatement callableStatement = connect.prepareCall(UPDATE_PUBLICACION_QUERY);
 
-            callableStatement.setInt(1, entity.getIdProducto());
+            callableStatement.setInt(1, entity.getId());
             callableStatement.setString(2, entity.getTitulo());
             callableStatement.setString(3, entity.getEstado());
             callableStatement.setInt(4, entity.getPrecio());
